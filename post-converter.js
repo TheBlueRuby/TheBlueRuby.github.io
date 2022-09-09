1afea06797e69f612638bfad4ecf16dc2a008283
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-09-09 21:57:45 +1000

    setup markdown

---
+++ post-converter.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+    var latestPost = 'posts/latest.md'
+
+    function loadFile() {
+        reader.open('get', latestPost, true); 
+        reader.onreadystatechange = displayContents;
+        reader.send(null);
+    }
+
+    function displayContents() {
+        if(reader.readyState==4) {
+            var mdText = reader.responseText;
+            console.log(mdText);
+            var converter = new showdown.Converter(),
+            html      = converter.makeHtml(mdText);
+
+            document.getElementById("latest-post").innerHTML = html;
+        }
+    }
+
+    loadFile();
+    displayContents();
+});
