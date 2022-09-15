4f0ef400d6c6e61e325313978e81f21995525254
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-09-15 21:53:16 +1000

    added aboutme page, added hidden theme

--- post-converter.js
+++ post-converter.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
     var latestPost = 'posts/latest.md'
 
     function loadFile() {
-        reader.open('get', latestPost, true); 
+        reader.open('get', latestPost, true);
         reader.onreadystatechange = displayContents;
         reader.send(null);
     }
 
     function displayContents() {
-        if(reader.readyState==4) {
+        if (reader.readyState == 4) {
             var mdText = reader.responseText;
             console.log(mdText);
             var converter = new showdown.Converter(),
-            html      = converter.makeHtml(mdText);
+                html = converter.makeHtml(mdText);
 
             console.log(html);
 
---
+++ themeswitcher.js
@@ -0,0 +1,67 @@
+
+var mite = '';
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    var themeSwitcher = document.getElementById("darkmode-toggler");
+
+    var localStorageTheme = localStorage.getItem("theme");
+
+    if (localStorageTheme == "light") {
+        themeSwitcher.checked = false;
+        changeTheme(localStorageTheme);
+    } else if (localStorageTheme == "mite") {
+        themeSwitcher.checked = true;
+        changeTheme(localStorageTheme);
+    } else {
+        themeSwitcher.checked = true;
+        changeTheme(localStorageTheme);
+    }
+
+    themeSwitcher.onchange = function () {
+        if (themeSwitcher.checked) {
+            changeTheme('dark');
+        } else {
+            changeTheme('light');
+        }
+    }
+
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.key.toLowerCase() == 'm') {
+        mite += 'm';
+    }
+    if (event.key.toLowerCase() == 'i') {
+        mite += 'i';
+    }
+    if (event.key.toLowerCase() == 't') {
+        mite += 't';
+    }
+    if (event.key.toLowerCase() == 'e') {
+        mite += 'e';
+    }
+    if (mite == 'mite') {
+        changeTheme(mite.toLowerCase());
+    } else if (mite.length >= 4) {
+        mite = '';
+    }
+});
+
+function changeTheme() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+
+    var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+    console.log("Changing theme to " + switchToTheme);
+
+    document.documentElement.setAttribute("data-theme", switchToTheme);
+    localStorage.setItem("theme", switchToTheme);
+}
+
+function changeTheme(switchToTheme) {
+
+    console.log("Changing theme to " + switchToTheme);
+
+    document.documentElement.setAttribute("data-theme", switchToTheme);
+    localStorage.setItem("theme", switchToTheme);
+}
